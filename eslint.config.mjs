114ad65257aba4f6764443b9327c7f53fa35725a
Diana Diaz <112323822+dee-diaz@users.noreply.github.com>
2025-11-21 08:38:26 -0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  globalIgnores(['webpack.*.js']),
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  eslintPluginPrettier,
+]);
