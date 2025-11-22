68680cdb4771eb89cf46147507e8336f11ebbe71
dee-diaz <diazthediablo@yahoo.com>
2025-11-22 17:36:42 -0300

    Responsive header with the search bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const toggleBtn = document.getElementById('tempToggle');
+const tempLabels = document.querySelectorAll('.temp-label');
+
+toggleBtn.addEventListener('click', () => {
+  toggleBtn.classList.toggle('active');
+
+  tempLabels.forEach((label) => {
+    label.classList.toggle('active');
+  });
+});
